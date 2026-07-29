0a564296ac491770ce67a8787c7d0f7bc2b056f1
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-30 00:18:44 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,28 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("✅ تم تسجيل الدخول بنجاح");
+
+        window.location.href = "chat.html";
+
+    } catch (error) {
+
+        if (error.code === "auth/invalid-credential") {
+            alert("البريد الإلكتروني أو كلمة المرور غير صحيحة");
+        } else {
+            alert(error.message);
+        }
+
+    }
+});
