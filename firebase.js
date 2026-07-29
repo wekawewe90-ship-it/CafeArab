c2ccea080ab9224d8b66693740f5f118ae328506
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-30 02:30:28 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
-// Firebase SDK
+// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+
+// Firebase Auth
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+// Firestore
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-// إعدادات مشروعك
+// إعدادات المشروع
 const firebaseConfig = {
   apiKey: "AIzaSyCc2LiujPKNiSaMHi6btRkfP-sB4DnRM5Q",
   authDomain: "cafearab-c6f0b.firebaseapp.com",
   projectId: "cafearab-c6f0b",
   storageBucket: "cafearab-c6f0b.firebasestorage.app",
   messagingSenderId: "155925331863",
-  appId: "1:155925331863:web:10a5f1959ccfdc56fc0ed2",
-  measurementId: "G-Y6MEL4XGVC"
+  appId: "1:155925331863:web:10a5f1959ccfdc56fc0ed2"
 };
 
 // تشغيل Firebase
