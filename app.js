65503a0415d8c29c54fd3f53f5d994c90ca0b326
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-29 19:23:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
+// ===========================
+// Cafe Arab
+// app.js
+// ===========================
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Cafe Arab Started");
+
+    // تأثير عند الضغط على الأزرار
+    const buttons = document.querySelectorAll(".btn");
+
+    buttons.forEach(button => {
+
+        button.addEventListener("click", function(e) {
+
+            e.preventDefault();
+
+            this.style.transform = "scale(0.97)";
+
+            setTimeout(() => {
+
+                this.style.transform = "scale(1)";
+
+            },150);
+
+            const text = this.innerText;
+
+            if(text.includes("ضيف")){
+                alert("🚀 قريبًا سيتم دخول الضيف.");
+            }
+
+            else if(text.includes("إنشاء")){
+                alert("📝 صفحة إنشاء الحساب قريبًا.");
+            }
+
+            else if(text.includes("تسجيل")){
+                alert("🔑 صفحة تسجيل الدخول قريبًا.");
+            }
+
+        });
+
+    });
+
+});
