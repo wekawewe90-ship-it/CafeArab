8e99ad2b48ab04852750742980587abc5097d165
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-30 02:34:41 +0300

    Update register.js

--- register.js
+++ register.js
@@ -1,20 +1,20 @@
 import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const form = document.getElementById("registerForm");
 
 form.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
-const confirmPassword = document.getElementById("confirmPassword").value.trim();
 
-if (password !== confirmPassword) {
-    alert("كلمتا المرور غير متطابقتين");
-    return;
-}
     try {
+
         await createUserWithEmailAndPassword(auth, email, password);
 
         alert("🎉 تم إنشاء الحساب بنجاح");
@@ -22,14 +22,11 @@ if (password !== confirmPassword) {
         window.location.href = "login.html";
 
     } catch (error) {
-        if (error.code === "auth/email-already-in-use") {
-    alert("البريد الإلكتروني مستخدم بالفعل");
-} else if (error.code === "auth/weak-password") {
-    alert("كلمة المرور يجب أن تكون 6 أحرف على الأقل");
-} else if (error.code === "auth/invalid-email") {
-    alert("البريد الإلكتروني غير صحيح");
-} else {
-    alert(error.message);
-        } 
+
+        alert(error.message);
+
+        console.error(error);
+
     }
+
 });
