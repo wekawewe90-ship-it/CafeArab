7aca8ad2f0adb890f44de3e55d3db3d96cea7377
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-30 02:21:06 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,7 +1,36 @@
-alert("✅ chat.js يعمل");
+import { auth, db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const messages = document.getElementById("messages");
+const input = document.getElementById("messageInput");
 const sendBtn = document.getElementById("sendBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const userName = document.getElementById("userName");
 
-sendBtn.addEventListener("click", () => {
-    alert("🎉 زر الإرسال يعمل");
+let currentUser = null;
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    currentUser = user;
+    userName.textContent = "👤 " + user.email;
+
+    console.log("تم تسجيل الدخول:", user.email);
 });
