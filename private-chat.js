a274f95e7692b0f15623fff37fb1c359a62a5d6c
wekawewe90-ship-it <wekawewe90@gmail.com>
2026-07-30 19:31:55 +0300

    Update private-chat.js

--- private-chat.js
+++ private-chat.js
@@ -1,1 +1,41 @@
+import { auth, db } from "./firebase.js";
 
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+const chatTitle = document.getElementById("chatTitle");
+
+// قراءة uid من الرابط
+const params = new URLSearchParams(window.location.search);
+const otherUid = params.get("uid");
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    // قراءة بيانات المستخدم الآخر
+    const docRef = doc(db, "users", otherUid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+        const data = docSnap.data();
+
+        chatTitle.innerHTML = "💬 " + data.name;
+
+    } else {
+
+        chatTitle.innerHTML = "المستخدم غير موجود";
+
+    }
+
+});
